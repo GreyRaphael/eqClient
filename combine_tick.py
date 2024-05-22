6d31702cca3d8d4daba6811f4f3b323b8033c185
ali_gewei <grey@pku.edu.cn>
2024-05-22 10:07:00 +0800

    finish combin_tick

---
+++ combine_tick.py
@@ -0,0 +1,70 @@
+import polars as pl
+import argparse
+import os
+
+
+def combiner_ym(year_month: int, in_dir: str, output_dir: str):
+    in_files = f"{in_dir}/{year_month}/*"
+    print("reading", in_files)
+
+    df = (
+        pl.scan_parquet(in_files)
+        .filter((pl.col("open") != 0) & (pl.col("last") != 0))
+        .select(
+            pl.col("code").cast(pl.UInt32),
+            (pl.col("date").cast(pl.Utf8) + pl.col("time").cast(pl.Utf8).str.pad_start(9, "0")).str.to_datetime("%Y%m%d%H%M%S%3f").alias("dt"),
+            pl.col("preclose").cast(pl.UInt32),
+            pl.col("open").cast(pl.UInt32),
+            pl.col("last").cast(pl.UInt32),
+            pl.col("num_trades").cast(pl.UInt32).fill_null(0),
+            pl.col("volume").cast(pl.UInt64).fill_null(0),
+            pl.col("total_ask_volume").cast(pl.UInt64).fill_null(0).alias("tot_av"),
+            pl.col("total_bid_volume").cast(pl.UInt64).fill_null(0).alias("tot_bv"),
+            pl.col("amount").cast(pl.UInt64).fill_null(0),
+            pl.col("ask_avg_price").cast(pl.UInt32).alias("avg_ap"),
+            pl.col("bid_avg_price").cast(pl.UInt32).alias("avg_bp"),
+            *[pl.col("ask_prices").list.get(i).cast(pl.UInt32).fill_null(0).alias(f"ap{i}") for i in range(10)],
+            *[pl.col("bid_prices").list.get(i).cast(pl.UInt32).fill_null(0).alias(f"bp{i}") for i in range(10)],
+            *[pl.col("ask_volumes").list.get(i).cast(pl.UInt32).fill_null(0).alias(f"av{i}") for i in range(10)],
+            *[pl.col("bid_volumes").list.get(i).cast(pl.UInt32).fill_null(0).alias(f"bv{i}") for i in range(10)],
+        )
+        .sort(["code", "dt"])
+        .collect()
+    )
+    print(df.null_count().select("code", "dt", "preclose", "open", "last", "num_trades", "volume", "tot_av", "tot_bv", "amount", "avg_ap", "avg_bp").to_dicts())
+
+    os.makedirs(output_dir, exist_ok=True)
+    out_file = f"{output_dir}/{year_month}.ipc"
+    df.write_ipc(out_file, compression="zstd")
+    print("===>finish", out_file)
+
+
+def gen_ym_list(ym_start: int, ym_end: int) -> list:
+    year_start = ym_start // 100
+    year_end = ym_end // 100
+    ym_list = [year * 100 + month for year in range(year_start, year_end + 1) for month in range(1, 13)]
+    return [ym for ym in ym_list if ym_start <= ym <= ym_end]
+
+
+def run_tick_combiner(args):
+    if args.etf:
+        in_dir = "tick/etf"
+        out_dir = "tick-etf"
+    else:
+        in_dir = "tick/stocks"
+        out_dir = "tick-stocks"
+
+    for year_month in gen_ym_list(args.ym_start, args.ym_end):
+        combiner_ym(year_month, in_dir, out_dir)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="tick parquets combiner")
+
+    parser.add_argument("--ym-start", type=int, required=True, help="start year month, 202201")
+    parser.add_argument("--ym-end", type=int, required=True, help="end year month, 202312")
+    parser.add_argument("--etf", action="store_true", help="flag, if set 'etf' else 'stocks'")
+    parser.set_defaults(func=run_tick_combiner)
+
+    args = parser.parse_args()
+    args.func(args)
