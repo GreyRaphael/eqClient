cbcf12fbaf9414464ef4d61a5bced3fbc53fdcf6
ali_gewei <grey@pku.edu.cn>
2024-05-31 13:49:07 +0800

    fix specified task

--- scheduler_tasks/task_specified.py
+++ scheduler_tasks/task_specified.py
@@ -1,26 +1,27 @@
 from collections import namedtuple
 from apscheduler.schedulers.blocking import BlockingScheduler
 import hq
+from utils import chatbot
 
 Args = namedtuple("Args", ["ym_start", "ym_end", "secu_type", "quote_type"])
 
 
 def job_worker(args):
     hq.process(args)
-    print("finish", args)
+    chatbot.send_msg(f"finish {args}")
 
 
 task_dict = {
-    "2024-05-31 15:07:00": Args(ym_start=201901, ym_end=202012, secu_type="etf", quote_type="tick"),
-    "2024-06-01 09:40:00": Args(ym_start=202101, ym_end=202212, secu_type="etf", quote_type="tick"),
-    "2024-06-02 09:45:00": Args(ym_start=202301, ym_end=202405, secu_type="etf", quote_type="tick"),
+    "2024-05-31 15:07:00": Args(ym_start=202101, ym_end=202206, secu_type="etf", quote_type="tick"),
+    "2024-06-01 09:51:00": Args(ym_start=202207, ym_end=202312, secu_type="etf", quote_type="tick"),
+    "2024-06-02 09:47:00": Args(ym_start=202401, ym_end=202404, secu_type="etf", quote_type="tick"),
 }
 
 
 scheduler = BlockingScheduler()
 for task_date in task_dict:
-    arg_np = task_dict[task_date]
-    scheduler.add_job(job_worker, "date", run_date=task_date, args=(arg_np,))
+    arg_namesp = task_dict[task_date]
+    scheduler.add_job(job_worker, "date", run_date=task_date, args=(arg_namesp,))
 
 for job in scheduler.get_jobs():
     print(job)
